63f2007d7da44eb81511a98c9bf56211996ca8cc
Sara Cotrim <sara.perroni@aluno.senai.br>
2025-04-24 08:02:50 -0300

    first commit

---
+++ FrontEnd/src/pages/Ranking.jsx
@@ -0,0 +1,12 @@
+function Ranking() {
+    return (
+        <div>
+            <h1>Ranking</h1>
+            <p>This is the Ranking Page.</p>
+        </div>
+    );
+}
+
+export default Ranking;
+
+// uma coleção é o local onde fica a tabela do firebase
